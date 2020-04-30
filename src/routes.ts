cf1724be39c720ca2976cf2f3ae55c2e92d27cbb
Françuel Soares <fs.francuelsoares@gmail.com>
2020-04-29 21:04:43 -0300

    # New - Typing Objects and Vectors

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,14 @@
 import {Request, Response} from 'express';
+import createUser from './services/CreateUser';
 
 export function helloWorld(request: Request, response: Response){
+
+    const user = createUser({
+      name: 'Françuel', 
+      email: 'fran@gmail.com', 
+      password: '123456',
+      techs: ['NodeJs', {title: 'ReactJS', experience: 100}]
+    });
+
   return response.json({message: 'Hello World'});
 }
